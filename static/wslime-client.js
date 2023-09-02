9db5a83d8b62555b26a5420d9f57b3212466622f
Teletrup <saletrapotasowatosyf@gmail.com>
2023-09-02 17:02:37 +0200

    client reads port from config

--- static/wslime-client.js
+++ static/wslime-client.js
@@ -1,25 +1,30 @@
-wslime = {
-  attrib: s => document.querySelector('script').getAttribute(s),
-  eval: async src => (async function (){}).constructor(src)(),
-  load: async url => wslime.eval(await (await fetch(url)).text()),
-};
-wslime.ws = new WebSocket(`ws://localhost:${wslime.attrib('port') ?? 8001}`)
+(async () => {
+
+  top.wslime = {
+    eval: async src => (async function (){}).constructor(src)(),
+    load: async url => wslime.eval(await (await fetch(url)).text()),
+  };
+
+  const config = (await (await fetch('config.json')).json())
+
+  const ws = new WebSocket(`ws://localhost:${config.ws_port ?? 8001}`)
 
 
-console.log('waiting for wslime connection');
-wslime.ws = 
-wslime.ws.onopen = () => console.log('wslime connected');
-wslime.ws.onclose = () => console.log('wslime disconnected');
+  console.log('waiting for wslime connection');
+  ws.onopen = () => console.log('wslime connected');
+  ws.onclose = () => console.log('wslime disconnected');
 
 
-wslime.ws.onmessage = async e => {
-  const indent = s => s.split('\n').map(x => '  ' + x).join('\n');
-  let msg = indent(e.data.trim());
-  msg = '>' + msg.slice(1);
-  console.log(msg)
-  let val = await wslime.eval(e.data);
-  console.log(val)
-}
+  ws.onmessage = async e => {
+    const indent = s => s.split('\n').map(x => '  ' + x).join('\n');
+    let msg = indent(e.data.trim());
+    msg = '>' + msg.slice(1);
+    console.log(msg)
+    let val = await wslime.eval(e.data);
+    console.log(val)
+  }
 
 
-wslime.load(wslime.attrib('init'));
+  wslime.load(config.init);
+
+})();
